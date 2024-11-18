0eea7325b769ca27d76c37dcb27b33604c142a29
bhagyashah05 <bhagyashah5122002@gmail.com>
2024-11-18 18:45:40 +0530

    basic structure

---
+++ src/main/java/com/bhagya/esdassignment/EsdassignmentApplication.java
@@ -0,0 +1,13 @@
+package com.bhagya.esdassignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EsdassignmentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EsdassignmentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bhagya/esdassignment/EsdassignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bhagya.esdassignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EsdassignmentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
